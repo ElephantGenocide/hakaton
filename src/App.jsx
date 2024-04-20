f1f68aa090047bc209123f900a740d1e9b80c633
Elephant.Genocide <elephant.genocide@gmail.com>
2024-04-20 12:33:56 +0300

    Started Formatting

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import {Header, Hero, Features, Content, Footer} from './components/index'
 function App() {
 
   return (
-    <div className="App">
+    <>
       <Header />
       <Hero />
       <Features />
       <Content />
       <Footer />
       <h1>Привет мам!</h1>
-    </div>
+    </>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './header.css'
+import logo from '../../assets/fingerpring.svg'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='navbar'>
+      <div className='navbar_logo'>
+        <img src={logo} alt='logo' />
+      </div>
+      <div className='navbar_container'>
+        <a href='#home'>Главная</a>
+        <a href='#home'>О нас</a>
+        <a href='#home'>Связь</a>
+      </div>
+      <div className='navbar_buttons'>
+        <button type='button'>Войти</button>
+        <button type='button'>Регистрация</button>
+      </div>
     </div>
   )
 }
