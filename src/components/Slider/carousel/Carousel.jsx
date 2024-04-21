ae2138d0c70a096531b500262531a27d2b181615
Elephant.Genocide <elephant.genocide@gmail.com>
2024-04-21 16:07:43 +0300

    slight-changes for a happy eye

--- src/components/Slider/carousel/Carousel.jsx
+++ src/components/Slider/carousel/Carousel.jsx
@@ -14,44 +14,42 @@ export const Carousel = ({ data }) => {
     setSlide(slide === 0 ? data.length - 1 : slide - 1);
   };
 
+  
   return (
     <>
-        <div className="carousel">
-          
-          {data.map((item, idx) => {
-            return (
-              <>
-
+      <div className="carousel">
+        {data.map((item, idx) => {
+          return (
+            <>
               <img
                 src={item.src}
                 alt={item.alt}
                 key={idx}
                 className={slide === idx ? "slide" : "slide slide-hidden"}
               />
-              
-              </>
-              
+            </>
+          );
+        })}
+        <button onClick={prevSlide} className="arrow arrow-left">
+          <SlArrowLeft />
+        </button>
+        <button onClick={nextSlide} className="arrow arrow-right">
+          <SlArrowRight />
+        </button>
+        <span className="indicators">
+          {data.map((_, idx) => {
+            return (
+              <button
+                key={idx}
+                className={
+                  slide === idx ? "indicator" : "indicator indicator-inactive"
+                }
+                onClick={() => setSlide(idx)}
+              ></button>
             );
           })}
-          <button onClick={prevSlide} className="arrow arrow-left"><SlArrowLeft /></button>
-          <button onClick={nextSlide} className="arrow arrow-right"><SlArrowRight /></button>
-
-          <span className="indicators">
-            {data.map((_, idx) => {
-              return (
-                <button
-                  key={idx}
-                  className={
-                    slide === idx ? "indicator" : "indicator indicator-inactive"
-                  }
-                  onClick={() => setSlide(idx)}
-                ></button>
-              );
-            })}
-          </span>
-        </div>
-      
+        </span>
+      </div>
     </>
-
   );
 };
