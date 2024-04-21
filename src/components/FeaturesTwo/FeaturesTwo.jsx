19da128daef086d983b8cad72e5626f42076ef8b
Winner0070 <ggg553782@gmail.com>
2024-04-21 09:47:06 +0300

    fuetwo2.0

--- src/components/FeaturesTwo/FeaturesTwo.jsx
+++ src/components/FeaturesTwo/FeaturesTwo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './featurestwo.css'
-
+import img123 from '../../assets/feut/png-clipart-computer-icons-service-business-report-management-business-blue-text.svg'
 
 
 const FeaturesTwo = () => {
@@ -9,16 +9,16 @@ const FeaturesTwo = () => {
       <div className="feutwo">
         <div className="feutwo_conteiner">
           <h1>СТИЛПОРТАЛ</h1>
-          <h2 className='shcont'>Онлайн-доступ к космической съемке и сервисам</h2>
+          <img src={img123} alt="img123" />
         </div>
         <div className="feutwo_conteiner_stolbic">
-          <p>Подбор архивной съемки от разных поставщиков в одном окне</p>
-          <p>Удобный заказ новой съемки на портале</p>
-          <p>Тематическая обработка и аналитика данных ДЗЗ</p>
-          <p>Работа с серией разновременных изображений</p>
-          <p>Онлайн-оплата для физических и юридических лиц</p>
+          <h2 className='shcont'>Онлайн-доступ к космической съемке и сервисам</h2>
+          <p>-Подбор архивной съемки от разных поставщиков в одном окне</p>
+          <p>-Удобный заказ новой съемки на портале</p>
+          <p>-Тематическая обработка и аналитика данных ДЗЗ</p>
+          <p>-Работа с серией разновременных изображений</p>
+          <p>-Онлайн-оплата для физических и юридических лиц</p>
         </div>
-        <h2 className='service'>Сервис</h2>
       </div>
     </div>
   )
